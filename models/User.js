180d435e12fd0638a499cb4f48f0ca835fa84b8e
justyeh <justyeh@163.com>
2017-06-14 09:39:48 +0800

    Create User.js

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ var User = function() {};
 
 User.prototype.list = function(callback) {
     var sql = "SELECT * FROM users";
-    db.pool(sql,[],function(err,results){
+    db.query(sql,[],function(err,results){
         if (err) {
             callback(true);
             return;
@@ -16,7 +16,7 @@ User.prototype.list = function(callback) {
 User.prototype.one = function(userID,callback) {
     var sql = "SELECT * FROM users where id = ?";
     //连接池
-    db.pool(sql,[userID],function(err,results){
+    db.query(sql,[userID],function(err,results){
         if (err) {
             callback(true);
             return;
